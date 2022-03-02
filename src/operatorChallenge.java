6f01701987c588d5ff7710bd80c1e6e304830975
Raymond Dugan <raymond.j.dugan@gmail.com>
2022-03-02 12:05:24 -0600

    completed operatorChallenge

---
+++ src/operatorChallenge.java
@@ -0,0 +1,12 @@
+public class operatorChallenge {
+    public static void main(String[] args) {
+        double twenty = 20.00d;
+        double eighty = 80.00d;
+        double total = (twenty + eighty) * 100d;
+        double remainder = total % 40.00d;
+        boolean hasRemainder = remainder == 0;
+        if (!hasRemainder) {
+            System.out.println("Got some remainder");
+        }
+    }
+}
