94965da8a54f3e16b6b4afe61158524af5598c11
Raymond Dugan <raymond.j.dugan@gmail.com>
2022-03-06 17:09:31 -0600

    Finished megaByteConverter challenge

---
+++ src/mega_bytes_convertor/megaByteConverter.java
@@ -0,0 +1,18 @@
+package mega_bytes_convertor;
+
+public class megaByteConverter {
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+        if (kiloBytes < 0){
+            System.out.println("Invalid Value");
+            return;
+        }
+        System.out.printf("%d KB = %d MB and %d KB%n", kiloBytes,
+                kiloBytes / 1024, kiloBytes % 1024);
+    }
+
+    public static void main(String[] args) {
+        printMegaBytesAndKiloBytes(2500);
+        printMegaBytesAndKiloBytes(-1024);
+        printMegaBytesAndKiloBytes(5000);
+    }
+}
