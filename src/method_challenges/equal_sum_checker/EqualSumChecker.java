3d6cf6d15e1f4cbf3435cd9cd594dd70186ee832
Raymond Dugan <raymond.j.dugan@gmail.com>
2022-03-07 15:09:10 -0600

    Completed EqualSumChecker

--- src/method_challenges/equal_sum_checker/EqualSumChecker.java
+++ src/method_challenges/equal_sum_checker/EqualSumChecker.java
@@ -1,4 +1,11 @@
 package method_challenges.equal_sum_checker;
 
 public class EqualSumChecker {
+    public static boolean hasEqualSum(int numberOne, int numberTwo, int numberThree) {
+        return (numberOne + numberTwo) == numberThree;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
