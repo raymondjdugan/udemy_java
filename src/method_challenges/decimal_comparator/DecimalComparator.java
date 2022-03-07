a1bbf0698282f87bc44cbf25ebcf66127ef5eb0f
Raymond Dugan <raymond.j.dugan@gmail.com>
2022-03-07 14:57:50 -0600

    Finished DecimalComparator

--- src/method_challenges/decimal_comparator/DecimalComparator.java
+++ src/method_challenges/decimal_comparator/DecimalComparator.java
@@ -1,5 +1,19 @@
 package method_challenges.decimal_comparator;
 
-public class DecimalComparator {
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 
+public class DecimalComparator {
+    public static boolean areEqualByThreeDecimalPlaces (double firstNumber, double secondNumber) {
+        DecimalFormat df = new DecimalFormat("#.###");
+        df.setRoundingMode(RoundingMode.DOWN);
+
+        return df.format(firstNumber).equals(df.format(secondNumber));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(areEqualByThreeDecimalPlaces(-3.1756, -3.175));
+        System.out.println(areEqualByThreeDecimalPlaces(3.0, 3.0));
+        System.out.println(areEqualByThreeDecimalPlaces(-3.123, 3.123));
+    }
 }
