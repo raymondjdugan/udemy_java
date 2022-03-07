9760531f31623c2d1f68ba3f782452d6e8c8db74
Raymond Dugan <raymond.j.dugan@gmail.com>
2022-03-07 15:20:36 -0600

    Finished TeenNumberChecker

--- src/method_challenges/teen_number_checker/TeenNumberChecker.java
+++ src/method_challenges/teen_number_checker/TeenNumberChecker.java
@@ -1,4 +1,11 @@
 package method_challenges.teen_number_checker;
 
 public class TeenNumberChecker {
+    public static boolean hasTeen(int t1, int t2, int t3) {
+        return t1 >= 13 && t1 <= 19 || t2 >= 13 && t2 <= 19 || t3 >= 13 && t3 <= 19;
+    }
+
+    public static boolean isTeen(int t1) {
+        return t1 >= 13 && t1 <=19;
+    }
 }
