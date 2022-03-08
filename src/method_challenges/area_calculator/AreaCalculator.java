c8f545c5cd1bc2e1118cc8a85d79e045d74dbce1
Raymond Dugan <raymond.j.dugan@gmail.com>
2022-03-08 10:55:44 -0600

    Finished the AreaCalculator

--- src/method_challenges/area_calculator/AreaCalculator.java
+++ src/method_challenges/area_calculator/AreaCalculator.java
@@ -1,5 +1,17 @@
 package method_challenges.area_calculator;
 
 public class AreaCalculator {
+    public static double area(double radius) {
+        if (radius < 0) {
+            return -1;
+        }
+        return Math.pow(radius, 2) * Math.PI;
+    }
 
+    public static double area(double x, double y) {
+        if (x < 0 || y < 0) {
+            return -1;
+        }
+        return x * y;
+    }
 }
