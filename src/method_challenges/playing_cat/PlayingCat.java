bb31df0650b31616ac3e7ec450641f9af70822b2
Raymond Dugan <raymond.j.dugan@gmail.com>
2022-03-08 12:12:13 -0600

    Finished PlayingCat

--- src/method_challenges/playing_cat/PlayingCat.java
+++ src/method_challenges/playing_cat/PlayingCat.java
@@ -1,5 +1,10 @@
 package method_challenges.playing_cat;
 
 public class PlayingCat {
-
+    public static boolean isCatPlaying(boolean summer, int temperature){
+        if (summer) {
+            return temperature >= 25 && temperature <= 45;
+        }
+        return temperature >= 25 && temperature <= 35;
+    }
 }
