edfd5de6489ac88362a007544fe8ac4fda24796a
Raymond Dugan <raymond.j.dugan@gmail.com>
2022-03-13 18:35:24 -0500

    Completed NumberInWord Coding Challenge

--- src/control_statements_and_flow/number_in_word/NumberInWord.java
+++ src/control_statements_and_flow/number_in_word/NumberInWord.java
@@ -1,4 +1,44 @@
 package control_statements_and_flow.number_in_word;
 
 public class NumberInWord {
+    public static void printNumberInWord(int wholeNumber) {
+        switch (wholeNumber) {
+            case 0:
+                System.out.println("ZERO");
+                break;
+            case 1:
+                System.out.println("ONE");
+                break;
+            case 2:
+                System.out.println("TWO");
+                break;
+            case 3:
+                System.out.println("THREE");
+                break;
+            case 4:
+                System.out.println("FOUR");
+                break;
+            case 5:
+                System.out.println("FIVE");
+                break;
+            case 6:
+                System.out.println("SIX");
+                break;
+            case 7:
+                System.out.println("SEVEN");
+                break;
+            case 8:
+                System.out.println("EIGHT");
+                break;
+            case 9:
+                System.out.println("NINE");
+                break;
+            default:
+                System.out.println("OTHER");
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
