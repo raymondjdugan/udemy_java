406b58c99d3abd99e80e668fdb8afba81ca1e974
Raymond Dugan <raymond.j.dugan@gmail.com>
2022-04-03 11:14:55 -0500

    Completed SimpleCalculator

---
+++ src/classes/sum_calculator/SimpleCalculator.java
@@ -0,0 +1,41 @@
+package classes.sum_calculator;
+
+public class SimpleCalculator {
+    private double firstNumber;
+    private double secondNumber;
+
+    public double getAdditionResult() {
+        return firstNumber + secondNumber;
+    }
+
+    public double getSubtractionResult() {
+        return firstNumber - secondNumber;
+    }
+
+    public double getMultiplicationResult() {
+        return firstNumber * secondNumber;
+    }
+
+    public double getDivisionResult() {
+        if (secondNumber == 0) {
+            return 0;
+        }
+        return firstNumber / secondNumber;
+    }
+
+    public void setFirstNumber(double firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+
+    public double getFirstNumber() {
+        return firstNumber;
+    }
+
+    public void setSecondNumber(double secondNumber) {
+        this.secondNumber = secondNumber;
+    }
+
+    public double getSecondNumber() {
+        return secondNumber;
+    }
+}
