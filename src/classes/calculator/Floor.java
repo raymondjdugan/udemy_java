2c863befc188e7e6f56cdb818530f55140dd0c5b
Raymond Dugan <raymond.j.dugan@gmail.com>
2022-04-04 10:16:06 -0500

    Completed Flooring Cost Calculator

---
+++ src/classes/calculator/Floor.java
@@ -0,0 +1,26 @@
+package classes.calculator;
+
+public class Floor {
+    private double length;
+    private double width;
+
+    public Floor(double length, double width) {
+        if (length < 0) {
+            this.length = 0;
+            this.width = width;
+        } else if (width < 0) {
+            this.length = length;
+            this.width = 0;
+        } else if (width < 0 && length < 0) {
+            this.length = 0;
+            this.width = 0;
+        } else {
+            this.length = length;
+            this.width = width;
+        }
+    }
+
+    public double getArea() {
+        return length * width;
+    }
+}
